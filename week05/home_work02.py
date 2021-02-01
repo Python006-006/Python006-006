76924d3011a053b8ea469bf40e9e2fd4b4deace5
wclcoke <saibeilingyuanfe@126.com>
2021-02-01 21:17:41 +0800

    updae_homework2

--- week05/home_work02.py
+++ week05/home_work02.py
@@ -5,19 +5,21 @@ import redis
 import time
 client = redis.Redis(host = '127.0.0.1', password = '')
 
-def sendsms(telephone_number: int, content: string, key=None):
+def sendsms(telephone_number, content, key=None):
     # 短信发送逻辑, 作业中可以使用 print 来代替
-    # 计算手机发送的次数
 
-    client.set(telephone_number, '0', nx=True)
-    print(content)
-    count_number = client.incr(telephone_number)
-    #计算时间
-    start_time = time.time()
-    client.set(telephone_number, start_time, nx=True)
+    if not client.get(telephone_number):
+        client.set(telephone_number, '1', 60, nx=True)
+        print('发送成功0')
 
+    elif int(client.get(telephone_number)) < 5 :
+        count_number = client.incr(telephone_number)
+        print('发送成功1')
 
-    
-    # 请实现每分钟相同手机号最多发送五次功能, 超过 5 次提示调用方,1 分钟后重试稍后
-    pass
-    print("发送成功")
+    else:
+        print('1 分钟内发送次数超过 5 次, 请等待 1 分钟')
+
+if __name__ == '__main__':
+    sendsms(14725836989,'hello')
+    sendsms(15936598789,'hello')
+
