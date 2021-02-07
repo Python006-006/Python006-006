0da463f09f7e059d1d2d7c3548aa5e05fdd18fe4
wclcoke <saibeilingyuanfe@126.com>
2021-02-07 23:51:48 +0800

    week_homework006

--- week06/NOTE.md
+++ week06/NOTE.md
@@ -1,1 +1,90 @@
 学习笔记
+class Human(object):
+    #静态字段
+    live = True
+    
+    def __init__(self, name ,age):
+
+        #普通字段
+
+        self.name = name
+        self.age = age
+
+
+man = Human('Adam', 8)
+woman = Human('Eve', 9)
+
+#有静态字段live属性
+print(Human.__dict__)
+#有普通字段，name属性
+print(man.__dict__)
+
+#实例可以使用静态字段，也可以使用普通字段
+print(man.name)
+print(man.live)
+
+woman.live = False
+
+print(woman.live)
+
+#类可以使用静态字段
+print(Human.live)
+print(woman.__dict__)
+print(man.__dict__)
+
+Human.sname= 'tainjia'
+
+
+
+class Human(object):
+    #静态字段
+    live = True
+    
+    def __init__(self, name ,age):
+
+        #普通字段
+
+        self.name = name
+        self.age = age
+
+
+man = Human('Adam', 8)
+woman = Human('Eve', 9)
+
+#有静态字段live属性
+print(Human.__dict__)
+#有普通字段，name属性
+print(man.__dict__)
+
+#实例可以使用静态字段，也可以使用普通字段
+print(man.name)
+print(man.live)
+
+woman.live = False
+
+print(woman.live)
+
+#类可以使用静态字段
+print(Human.live)
+print(woman.__dict__)
+print(man.__dict__)
+
+#可以为类添加静态属性
+Human.sname= 'tainjia'
+
+print(Human.__dict__)
+
+TEST_name = Human('李四', 19)
+TEST_name.sname = '22'
+print(TEST_name.__dict__)
+
+print(dir(TEST_name))
+print('*'*20)
+print(dir(man))
+print(man.__dict__)
+
+#内置类型不能添加属性和方法
+setattr(list,  'newattr', 'value')  #相当于：list.newattr = value
+
+
+
---
+++ week06/home_work06.py
@@ -0,0 +1,85 @@
+
+class Animal():
+
+    def __init__(self, animal_type, somatotype, nature):
+        '''
+        动物类要求定义“类型”、“体型”、“性格”、“是否属于凶猛动物”四个属性，是否属于凶猛动物的判断标准是：“体型 >= 中等”并且是“食肉类型”同时“性格凶猛”。
+        '''
+        self.animal_type = type  
+        self.somatotype = somatotype
+        self.nature = nature
+
+    @property
+    def ls_fierce(self):
+        return True if self.type == '食肉' and (self.size == '中' or self.size == '大') and self.character == '性格凶猛' else False
+
+
+class Cat(Animal):
+
+    sound = '喵喵喵。。。'
+
+    def __init__(self, name ,animal_type, somatotype, nature):
+        super().__init__(animal_type, somatotype, nature)
+        self.name = name 
+
+    @property
+    def is_pet(self):
+        return False if self.ls_fierce else True
+        
+
+
+class Dog(Animal):
+    sound = '汪汪汪。。。'
+
+    def __init__(self, name ,animal_type, somatotype, nature):
+        super().__init__(animal_type, somatotype, nature)
+        self.name = name 
+
+    @property
+    def is_pet(self):
+        return False if self.ls_fierce else True
+
+class Zoo(object):
+    
+
+    def __init__(self,name):
+        self.name = name 
+        self.__animals = dict()
+    
+    def add_animal(self, animal):
+        animal_name = animal.__class__.__name__
+
+        if animal_name in self.__animals:
+
+            print(f"【{animal_name}】--【{animal.name}】已存在，请勿重复添加~")
+
+        else:
+            self.__animals[animal_name] = animal
+
+
+if __name__ == '__main__':
+
+
+    '''
+    实例化动物园
+    z = Zoo('时间动物园')
+    实例化一只猫，属性包括名字、类型、体型、性格
+    cat1 = Cat('大花猫 1', '食肉', '小', '温顺')
+    增加一只猫到动物园
+    z.add_animal(cat1)
+    动物园是否有猫这种动物
+    have_cat = hasattr(z, 'Cat')
+
+    '''
+
+    # 实例化动物园
+    z = Zoo('时间动物园')
+
+    # 实例化两只猫，属性包括名字、类型、体型、性格
+    cat1 = Cat('大花猫1', '食肉', '小', '温顺')
+
+    # 增加1只猫到动物园
+    z.add_animal(cat1)
+
+    # 动物园是否有猫这种动物
+    has_cat = hasattr(z, 'Cat')
